7fd972367c83099cd60f430489fe1acd4eb53ef8
Mohamed Kalmoua <mohamed_kalmoua@hotmail.com>
2022-04-21 19:46:43 +0200

    Simple example how to retrieve webpage content.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Retrieving webpage ...");
+
+            string content = await GetWebpageContent(@"https://www.microsoft.com");
+            Console.WriteLine($"Content size: {content.Length}");
+            Console.WriteLine($"Content: {content}");
+
+            Console.ReadKey();
         }
+
+        private static Task<string> GetWebpageContent(string Url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadStringTaskAsync(Url);
+        }
+
     }
 }
