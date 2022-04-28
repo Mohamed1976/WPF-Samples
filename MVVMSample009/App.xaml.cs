ae06df32910d2ea2fa4ede910223edc0797b734c
Mohamed Kalmoua <mohamed_kalmoua@hotmail.com>
2022-04-28 21:32:19 +0200

    Added mvvm samples

---
+++ MVVMSample009/App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MVVMSample009.Models;
+using System.Windows;
+using IOC = Microsoft.Toolkit.Mvvm.DependencyInjection.Ioc;
+
+namespace MVVMSample009
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            IOC.Default.ConfigureServices(
+                new ServiceCollection()
+                .AddSingleton<IFoo, Foo>()
+                .BuildServiceProvider());
+
+            //IServiceCollection sc = new ServiceCollection();
+            //sc.AddSingleton<IFoo, Foo>();
+            //System.IServiceProvider provider = sc.BuildServiceProvider();
+            //IOC.Default.ConfigureServices(provider);
+        }
+    }
+}
