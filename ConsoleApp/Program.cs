2c66d39e61015ea3a88ab4bd41651be44e81d02e
Mohamed Kalmoua <mohamed_kalmoua@hotmail.com>
2022-06-29 15:03:10 +0200

    Get data from KVK

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -8,9 +8,12 @@ namespace ConsoleApp
     {
         static async Task Main(string[] args)
         {
+            //KVK has a webservice which can be used to retrieve company data
+            //https://www.kvk.nl/zoeken/handelsregister/?handelsnaam=disq&kvknummer=&straat=&postcode=&huisnummer=&plaats=rotterdam&hoofdvestiging=1&rechtspersoon=1&nevenvestiging=1&zoekvervallen=0&zoekuitgeschreven=1&start=0
             Console.WriteLine("Retrieving webpage ...");
+            string url = @"https://www.kvk.nl/zoeken/handelsregister/?handelsnaam=Kadaster&plaats=Arnhem"; 
 
-            string content = await GetWebpageContent(@"https://www.microsoft.com");
+            string content = await GetWebpageContent(url);
             Console.WriteLine($"Content size: {content.Length}");
             Console.WriteLine($"Content: {content}");
 
